097ae131f391ee320b74e4df4dcfc654b2820b62
Deepika B <deepika.b@dxc.com>
2020-08-13 11:43:59 +0530

    added env variable

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 const _ = require("lodash");
 
 
@@ -10,14 +12,18 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true,   useFindAndModify: false });
 
-const userSchema = {
+
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-}
+});
 
 const User = new mongoose.model("User", userSchema);
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ["password"]  });
 
 app.get("/", function(req, res){
   res.render("home");
